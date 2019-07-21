33929dbf1502b5e14b8b4a6a15dddedc3df8ae87
JEverett <jonathan@jonathaneverettcreative.com>
2019-07-20 21:59:19 -0500

    Created foundation for new project working with React, React-Router, and an Express server. Copied some files from previous projects to refactor for use here. Need to come back tomorrow to flesh out the components and UI.

---
+++ client/src/components/Grid/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// Exporting the Container, Row, and Col components from this file
+
+// Bootstrap container w/o worrying about class names
+export function Container({ fluid, children }) {
+  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+// Bootstrap row w/o having to think about class names
+export function Row({ fluid, children }) {
+  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+// Col component for using bootstrap columns of varying sizes w/ less syntax
+export function Col({ size, children }) {
+  return (
+    <div
+      className={size
+        .split(" ")
+        .map(size => "col-" + size)
+        .join(" ")}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>Nerdy Translator</h1>
+                </Jumbotron>
+            </div>
+        )
+    }
+
+}
+
+export default Home;
